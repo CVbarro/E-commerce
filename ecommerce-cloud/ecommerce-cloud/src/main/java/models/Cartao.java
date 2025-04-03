ce85510f2c7fb82feab16272cdd2b1692527d2cd
César Viana Barros <126491073+CVbarro@users.noreply.github.com>
2025-04-03 20:03:42 -0300

    Commit principal

---
+++ ecommerce-cloud/ecommerce-cloud/src/main/java/models/Cartao.java
@@ -0,0 +1,26 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity (name = ("cartao"))
+public class Cartao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String numero;
+
+    @Column
+    private LocalDateTime dtExpiracao;
+
+    @Column
+    private String cvv;
+
+    @Column
+    private Double saldo;
+}
